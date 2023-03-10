e5dada864c8c0aaa173366acfab99748d671f333
Oleksandr Styhar <ostyhar@softserveinc.com>
2023-03-10 15:11:32 +0100

    start

--- main.tf
+++ main.tf
@@ -2,50 +2,9 @@ data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
 
-# data "aws_iam_policy_document" "assume_role_policy" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-
-#     principals {
-#       type        = "Service"
-#       identifiers = [
-#         "transfer.amazonaws.com",
-#         "ec2.amazonaws.com"
-#       ]
-#     }
-#   }
-# }
-
-# resource "aws_iam_role" "main" {
-#   name               = var.iam_role_name
-#   description        = var.iam_role_description
-#   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
-#   tags               = var.tags
-# }
-
-# data "aws_iam_policy_document" "role_policy" {
-#   statement {
-#     actions = [
-#       "logs:DescribeLogStreams",
-#       "logs:CreateLogGroup",
-#       "logs:CreateLogStream",
-#       "logs:PutLogEvents",
-#     ]
-#     resources = [
-#       format("arn:aws:logs:%s:%s:*", data.aws_region.current.name, data.aws_caller_identity.current.account_id)
-#     ]
-#   }
-# }
-
-# resource "aws_iam_role_policy" "main" {
-#   name   = var.iam_role_name
-#   role   = aws_iam_role.main.name
-#   policy = data.aws_iam_policy_document.role_policy.json
-# }
-
 resource "aws_transfer_server" "main" {
   identity_provider_type = "SERVICE_MANAGED"
-  logging_role           = aws_iam_role.main.arn
+  logging_role           = aws_iam_role.logs_role.arn
 
   endpoint_type = var.endpoint_type
   dynamic "endpoint_details" {
